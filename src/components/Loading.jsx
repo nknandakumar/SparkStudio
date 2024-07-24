1d1940dcd7f97f10d0a218d5421da913df2ccd61
Parantapa Kapi-Dhwaja <you@example.com>
2024-07-25 01:55:38 +0530

    25/07/2024 1:55 11th commit

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,11 +2,10 @@ import thunder from "/assets/favicon.svg"
 
 const Loading = () => {
   return (
-    <div className=" flex justify-center items-center pt-32 bg-zinc-300 h-screen ">
-      <div className="p-3 animate-spin drop-shadow-2xl bg-gradient-to-bl from-pink-400 via-purple-400 to-indigo-600 md:w-48 md:h-48 h-32 w-32 aspect-square rounded-full">
-        <div className="rounded-full h-full w-full bg-slate-100  background-blur-md">
-          <img className=" w-26 h-26 rounded-full p-2" src={thunder} alt="thunder Logo" />  
-          
+    <div className=" flex justify-center items-center  bg-zinc-50 h-screen ">
+      <div className="p-3 animate-spin h-16 w-16  border-8 border-dashed rounded-full border-blue-600 ">
+        <div className="rounded-full h-full w-full bg-slate-100 flex justify-center items-center  background-blur-md">
+           ⚡
         </div>
       </div>
     </div>
